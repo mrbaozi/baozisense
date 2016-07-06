407e556d8f95adaabaf456c35899dd582bcebd00
mrbaozi <mrbaozi@me.com>
2016-07-07 00:04:14 +0200

    update repo

--- update_rrd.sh
+++ update_rrd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ssh root@planetbaozi.ddns.net 'rrdtool dump sensehat_data.rrd > sensehat_data.xml'
-scp root@planetbaozi.ddns.net:/root/sensehat_data.xml /home/yannick/baozisense_dev
-rrdtool restore /home/yannick/baozisense_dev/sensehat_data.xml /home/yannick/baozisense_dev/sensehat_data.rrd -f
+# scp root@planetbaozi.ddns.net:/root/sensehat_data.xml /home/yannick/baozisense_dev
+# rrdtool restore /home/yannick/baozisense_dev/sensehat_data.xml /home/yannick/baozisense_dev/sensehat_data.rrd -f
+scp root@planetbaozi.ddns.net:/root/sensehat_data.xml /Users/yannick/Desktop/baozisense_dev
+rrdtool restore /Users/yannick/Desktop/baozisense_dev/sensehat_data.xml /Users/yannick/Desktop/baozisense_dev/sensehat_data.rrd -f
